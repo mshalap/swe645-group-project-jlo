d21ea1d549abd15c9e8bc0e6f0865547b24bb791
Margaret Redman <mshalap@masonlive.gmu.edu>
2020-11-01 09:35:16 -0500

    adding components

--- HW3/student-survey/src/app/app.module.ts
+++ HW3/student-survey/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TakeSurveyComponent } from './take-survey/take-survey.component';
+import { DisplayResultsComponent } from './display-results/display-results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TakeSurveyComponent,
+    DisplayResultsComponent
   ],
   imports: [
     BrowserModule,
