a7df7c25159651dfcfa82596892a846402a69909
Margaret Redman <mshalap@masonlive.gmu.edu>
2020-11-01 11:36:06 -0500

    setting up initial views, really ugly, just trying to get something working

--- HW3/student-survey/src/app/app-routing.module.ts
+++ HW3/student-survey/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DisplayResultsComponent } from './display-results/display-results.component';
+import { TakeSurveyComponent } from './take-survey/take-survey.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'take-survey', component: TakeSurveyComponent },
+  { path: 'display-results', component: DisplayResultsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
