82629fded98762852139802110a415cb87481699
jlokastle <33757428+jlokastle@users.noreply.github.com>
2020-11-01 11:58:41 -0500

    more stuff

--- HW3/rest/src/swe645/StudentSurvey.java
+++ HW3/rest/src/swe645/StudentSurvey.java
@@ -1,8 +1,11 @@
 package swe645;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import swe645.entities.*;
 
@@ -10,7 +13,7 @@ import swe645.entities.*;
 public class StudentSurvey {
 
 	/*
-	 * Test code*/
+	 * Test code
 	@GET
 	//public List<Survey> get() {
 	public String get() {
@@ -18,9 +21,9 @@ public class StudentSurvey {
 		return "Hello World";
 	}
 	
+	*/
 	
-	
-	/*@GET
+	@GET
 	public List<Survey> get() {
 		List<Survey> retval = new ArrayList<Survey>();
 		return retval;
@@ -28,9 +31,35 @@ public class StudentSurvey {
 	
 	@POST
 	@Consumes("applicatoin/x-www-form-urlencoded")
-	public void postdata(@FormParam("blah") int blah
-						
-						) {
+	public Response postdata(
+			@FormParam("First Name") String fname,
+			@FormParam("Last Name") String lname,
+			@FormParam("Street Address") String addr,
+			@FormParam("City") String city,
+			@FormParam("State") String state,
+			@FormParam("Zip Code") String zip,
+			@FormParam("Telephone Number") String tele,
+			@FormParam("Email Address") String email,
+			@FormParam("Survey Date") Date date,
+			@FormParam("students") String students,
+			@FormParam("location") String location,
+			@FormParam("campus") String campus,
+			@FormParam("dorm") String dorm,
+			@FormParam("atmosphere") String atmosphere,
+			@FormParam("sports") String sports,
+			@FormParam("interest") String interest,
+			@FormParam("option") String recommendation,
+			@FormParam("Raffle") String raffle,
+			@FormParam("Additional Comments") String comments
+			){
 		
-	}*/
+		if(fname.isEmpty() || lname.isEmpty() || addr.isEmpty() || city.isEmpty() ||
+				state.isEmpty() || zip.isEmpty() || tele.isEmpty() || email.isEmpty()) {
+			return Response.status(Status.BAD_REQUEST).entity("Required field missing").build();
+		}
+			
+		
+		
+		return Response.created(null).build();
+	}
 }
