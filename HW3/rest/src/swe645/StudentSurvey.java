22780ded4c41919086cb6b9ef547567aa4f7f9af
jlokastle <33757428+jlokastle@users.noreply.github.com>
2020-11-07 17:39:46 -0500

    Updates to get us running

--- HW3/rest/src/swe645/StudentSurvey.java
+++ HW3/rest/src/swe645/StudentSurvey.java
@@ -10,6 +10,7 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.transaction.UserTransaction;
 
 import javax.ejb.*;
+import javax.naming.InitialContext;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -20,11 +21,14 @@ import swe645.entities.*;
 @Path("survey")
 public class StudentSurvey{
 
+	public static boolean isNullOrBlank(String param) { 
+	    return param == null || param.trim().length() == 0;
+	}
+	
+	
 	@PersistenceContext(name = "HW3")
 	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("HW3");
 
-	@Resource
-	private UserTransaction utx;
 	
 	
 	/*
@@ -39,7 +43,7 @@ public class StudentSurvey{
 	*/
 	
 	@GET
-	@Produces("application/xml")
+	@Produces("application/json")
 	public List<Survey> get() {
 		EntityManager em = emf.createEntityManager();
 		CriteriaQuery<Survey> cq = em.getCriteriaBuilder().createQuery(Survey.class);
@@ -48,74 +52,81 @@ public class StudentSurvey{
 	}
 	
 	@POST
-	@Consumes("application/x-www-form-urlencoded")
-	public Response postdata(
-			@FormParam("First Name") String fname,
-			@FormParam("Last Name") String lname,
-			@FormParam("Street Address") String addr,
-			@FormParam("City") String city,
-			@FormParam("State") String state,
-			@FormParam("Zip Code") String zip,
-			@FormParam("Telephone Number") String tele,
-			@FormParam("Email Address") String email,
-			@FormParam("Survey Date") Date date,
-			@FormParam("students") String students,
-			@FormParam("location") String location,
-			@FormParam("campus") String campus,
-			@FormParam("dorm") String dorm,
-			@FormParam("atmosphere") String atmosphere,
-			@FormParam("sports") String sports,
-			@FormParam("interest") String interest,
-			@FormParam("option") String recommendation,
-			@FormParam("Raffle") String raffle,
-			@FormParam("Additional Comments") String comments
-			){
-		
-		if(fname.isEmpty() || lname.isEmpty() || addr.isEmpty() || city.isEmpty() ||
-				state.isEmpty() || zip.isEmpty() || tele.isEmpty() || email.isEmpty()) {
-			return Response.status(Status.BAD_REQUEST).entity("Required field missing").build();
-		}
-		
-			
-		//create entity and persist.
-		Survey s = new Survey();
-		s.setFname(fname);
-		s.setLname(lname);
-		s.setAddr(addr);
-		s.setCity(city);
-		s.setState(state);
-		s.setZip(zip);
-		s.setTele(tele);
-		s.setEmail(email);
-		s.setDate(date);
-		s.setStudents(students);
-		s.setLocation(location);
-		s.setCampus(campus);
-		s.setDorm(dorm);
-		s.setAtmosphere(atmosphere);
-		s.setSports(sports);
-		s.setInterest(interest);
-		s.setRecommendation(recommendation);
-		s.setRaffle(raffle);
-		s.setComments(comments);
-		
-		EntityManager em = emf.createEntityManager();
+	@Consumes("application/json")
+	public Response postdata(Survey s){
 		try {
-			utx.begin();
-			em.persist(s);
-			utx.commit();
-			
-		} catch (Exception e) {
-			try {
-				utx.rollback();
-			} catch (Exception e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+			if(isNullOrBlank(s.getFname())|| isNullOrBlank(s.getFname()) || isNullOrBlank(s.getAddr()) || isNullOrBlank(s.getCity()) ||
+					isNullOrBlank(s.getState()) || isNullOrBlank(s.getZip()) || isNullOrBlank(s.getTele()) || isNullOrBlank(s.getEmail())) {
+				return Response.status(Status.BAD_REQUEST).entity("Required field missing").build();
 			}
-			return Response.status(Status.INTERNAL_SERVER_ERROR)
+			
+				
+			//create entity and persist.
+			/*Survey s = new Survey();
+			s.setFname(fname);
+			s.setLname(lname);
+			s.setAddr(addr);
+			s.setCity(city);
+			s.setState(state);
+			s.setZip(zip);
+			s.setTele(tele);
+			s.setEmail(email);
+			s.setDate(date);
+			s.setStudents(students);
+			s.setLocation(location);
+			s.setCampus(campus);
+			s.setDorm(dorm);
+			s.setAtmosphere(atmosphere);
+			s.setSports(sports);
+			s.setInterest(interest);
+			s.setRecommendation(recommendation);
+			s.setRaffle(raffle);
+			s.setComments(comments);*/
+			
+			/*Survey s = new Survey();
+			s.setFname("J");
+			s.setLname("Lopez");
+			s.setAddr("youwish");
+			s.setCity("hoville");
+			s.setState("va");
+			s.setZip("22044");
+			s.setTele("8675309");
+			s.setEmail("jlo@theblock.org");
+			s.setDate(null);
+			s.setStudents(null);
+			s.setLocation(null);
+			s.setCampus(null);
+			s.setDorm(null);
+			s.setAtmosphere(null);
+			s.setSports(null);
+			s.setInterest(null);
+			s.setRecommendation(null);
+			s.setRaffle(null);
+			s.setComments(null);*/
+			
+			EntityManager em = emf.createEntityManager();
+			EntityTransaction et = em.getTransaction();
+			
+			try {
+				et.begin();
+				em.persist(s);
+				et.commit();
+		
+			} catch (Exception e) {
+				try {
+					et.rollback();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				return Response.status(Status.NOT_ACCEPTABLE)
+						.entity(e.getMessage()).build();
+			}
+		
+		}catch(Exception e) {
+			return Response.status(Status.NOT_FOUND)
 					.entity(e.getMessage()).build();
 		}
-		
 		return Response.created(null).build();
 	}
 }
